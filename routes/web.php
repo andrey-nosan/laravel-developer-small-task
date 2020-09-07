e82b93240dcaa6f85ffba04c1bb40e242e50b7a5
andrey-nosan <andrey.nosan@gmail.com>
2020-09-07 17:29:28 +0300

    updated web routes;

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('message.index');
+})->name('home');
+
 Route::get('/message', 'MessageController@index')->name('message.index');
